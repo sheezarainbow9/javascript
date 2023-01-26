d82053dad0a687dfc522ab9405bf32f0db81efa4
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-01-26 13:38:22 -0300

    Horas do dia

--- ex014/script.js
+++ ex014/script.js
@@ -3,5 +3,16 @@ function carregar() {
   var img = document.getElementById("imagem");
   var data = new Date();
   var hora = data.getHours();
-  msg.innerHTML(`Agora são ${hora} horas.`);
+  //var hora = 13;
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  if (hora >= 0 && hora < 12) {
+    img.src = "manha.png";
+    document.body.style.background = '#e2cd4f';
+  } else if (hora >= 12 && hora <= 18) {
+    img.src = "tarde.png";
+    document.body.style.background = '#E5BA73';
+  } else {
+    img.src = "noite.png";
+    document.body.style.background = '#2C3333';
+  }
 }
