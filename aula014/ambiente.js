8d296fa56f66f236b0ea6461e614f5d0d45a9dd6
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-01-29 10:09:58 -0300

    Repetições 2

---
+++ aula014/ambiente.js
@@ -0,0 +1,22 @@
+/* Repetições 2 
+
+
+var cont = 1;
+while (cont <= 6) {
+    console.log(`Passo ${cont}`);
+    cont++
+}
+
+É a mesma coisa que:
+
+
+for (var cont = 1; cont <= 6; cont++) {
+    console.log(cont);
+}
+*/
+
+console.log('Vai começar...');
+for (var cont = 1; cont <= 6; cont++) {
+    console.log(cont);
+}
+console.log('FIM!');
