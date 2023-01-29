e5b82209d496d26ab153d81d013accd265321a21
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-01-29 09:36:30 -0300

    Repetições em JS

---
+++ aula013/ambiente.js
@@ -0,0 +1,14 @@
+/* Repetições */
+
+/*var cont = 1;
+while (cont < 6) {
+    console.log("Tudo bem?");
+    cont++
+}
+*/
+
+var cont = 1;
+do {
+  console.log(`Passo ${cont}`);
+  cont++;
+} while (cont <= 6);
